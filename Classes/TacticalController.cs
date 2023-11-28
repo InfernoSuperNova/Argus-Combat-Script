bef25ae204b1adace4aab0ff82f763ff43d802d4
Gxaps <36036803+InfernoSuperNova@users.noreply.github.com>
2023-11-28 14:22:24 +1300

    Tactical controller, framework for switching up behavior in different situations

--- Classes/TacticalController.cs
+++ Classes/TacticalController.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using Sandbox.ModAPI.Ingame;
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Data;
+using VRageMath;
+using IngameScript.Classes.Behaviors;
+using Sandbox.Game.Entities.Cube;
 
 namespace IngameScript.Classes
 {
@@ -10,5 +12,41 @@ namespace IngameScript.Classes
     //The brains of the ship, chooses what data to supply to the locomotion segment of the ship. 
     public class TacticalController
     {
+
+        Behavior currentBehavior;
+        ShipControl shipControl;
+        Turrets turrets;
+        MyGridProgram program;
+        IMyTerminalBlock reference;
+        public TacticalController(List<IMyLargeTurretBase> turrets, ShipControlInitializationData shipControlInitializationData, IMyTerminalBlock reference) {
+            program = shipControlInitializationData.program;
+            this.reference = reference;
+            this.turrets = new Turrets(turrets);
+            currentBehavior = new SpinToWin(program);
+            shipControl = new ShipControl(shipControlInitializationData);
+        }
+
+        public void Update()
+        {
+
+
+            TurretReturnData target = turrets.GetTarget(TargetingType.CenterOfMass);
+
+            if (target.position != Vector3D.Zero)
+            {
+                currentBehavior = new SpinToWin(program);
+            }
+            else
+            {
+                currentBehavior = new Behavior();
+            }
+            var updateData = currentBehavior.UpdateData;
+            updateData.aimPositionTargetPos = target.position;
+            updateData.selfVelocity = reference.CubeGrid.LinearVelocity;
+            updateData.targetVelocity = Vector3D.Zero;
+            updateData.projectileVelocity = 2000;
+
+            shipControl.Update(currentBehavior.Flags, updateData);
+        }
     }
 }
