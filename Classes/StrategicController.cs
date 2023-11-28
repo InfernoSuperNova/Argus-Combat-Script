c8295dc4507a0b68657b1a9022b5add631fd9127
Gxaps <36036803+InfernoSuperNova@users.noreply.github.com>
2023-11-28 14:22:39 +1300

    Strategic controller, for co ordination

--- Classes/StrategicController.cs
+++ Classes/StrategicController.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using IngameScript.Classes.Behaviors;
 namespace IngameScript.Classes
 {
     //The operational hierarchy amongst drones. From here, one drone is elected from the leader, and any group calculations that need to be done will be routed to that drone
