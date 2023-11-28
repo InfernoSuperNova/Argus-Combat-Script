2085ee7ca3c3bbeceb0df17815c9ccd048b5229a
Gxaps <36036803+InfernoSuperNova@users.noreply.github.com>
2023-11-28 14:23:04 +1300

    Default behavior

---
+++ Classes/Behaviors/Behavior.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IngameScript.Classes.Behaviors
+{
+    public class Behavior
+    {
+        public virtual SCFlags Flags { get; set; } = SCFlags.None;
+        public ShipControlUpdateData UpdateData = new ShipControlUpdateData();
+    }
+}
