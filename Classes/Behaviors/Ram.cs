26fd7800f25943acbdd0d9fe905713bc63bec2af
Gxaps <36036803+InfernoSuperNova@users.noreply.github.com>
2023-11-28 14:23:16 +1300

    Ram behavior

---
+++ Classes/Behaviors/Ram.cs
@@ -0,0 +1,21 @@
+﻿using Sandbox.ModAPI.Ingame;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VRageRender.Messages;
+using IngameScript.Classes.Behaviors;
+namespace IngameScript.Classes.Behaviors
+{
+    public class Ram : Behavior
+    {
+        public override SCFlags Flags { get; set; } = SCFlags.PositionAroundTarget | SCFlags.UnifiedTargetAndRefererencePosition | SCFlags.UseManeuverThrustForAchievingDistance | SCFlags.RollToCancelVelocity;
+        public Ram(MyGridProgram program)
+        {
+            UpdateData = new ShipControlUpdateData();
+            UpdateData.requiredDistance = -100;
+            UpdateData.cancelThrustDir = ThrusterDir.Up;
+        }
+    }
+}
